514da235649a3e64a878bf32ab994d2d25fe634f
unknown <hernandezgalvezalejandro@gmail.com>
2024-12-02 21:56:21 -0600

    rutas

---
+++ project/first.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "<h1>Página de inicio</h1>"
+
+#string
+#int
+#float
+#path
+#uuid
+@app.route("/hello/") 
+@app.route("/hello/<string:name>") 
+@app.route("/hello/<string:name>/<int:age>") #variables en rutas
+def hello(name = None, age = None):
+    if name == None and age == None:
+        return "<h1>Hola, mundo!</h1>"
+    elif age == None:
+        return f"<h1>Hola, {name}!</h1>"
+    else:
+        return f"<h1>Hola {name}! el doble de tu edad es {age * 2}</h1>"
+
+from markupsafe import escape
+
+@app.route("/code/<path:code>")
+def code(code):
+    return f"<code>{escape(code)}</code>" # se anula la ejecución de código
