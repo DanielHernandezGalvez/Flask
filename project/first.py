0a7f1785bee8d4a6918834dc5599b020a1bf8c75
unknown <hernandezgalvezalejandro@gmail.com>
2024-12-08 14:30:03 -0600

    navegación con urls

--- project/first.py
+++ project/first.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -19,8 +19,10 @@ def repeat(string, number):
 from datetime import datetime
 
 @app.route("/")
-@app.route("/index")
 def index():
+    print(url_for("index"))
+    print(url_for("hello", name = "print('dani)"))
+    print(url_for("code", code = "print('Hola')"))
     name = "kramer"
     friends = ["Daniel", "Alexis", "Juan"]
     date = datetime.now()
@@ -39,14 +41,16 @@ def index():
 #uuid
 @app.route("/hello/") 
 @app.route("/hello/<string:name>") 
-@app.route("/hello/<string:name>/<int:age>") #variables en rutas
-def hello(name = None, age = None):
-    if name == None and age == None:
-        return "<h1>Hola, mundo!</h1>"
-    elif age == None:
-        return f"<h1>Hola, {name}!</h1>"
-    else:
-        return f"<h1>Hola {name}! el doble de tu edad es {age * 2}</h1>"
+@app.route("/hello/<string:name>/<int:age>") 
+@app.route("/hello/<string:name>/<int:age>/<email>") #variables en rutas
+def hello(name = None, age = None, email = None):
+   my_data = {
+       "name": name,
+       "age": age,
+       "email": email
+   } 
+   return render_template('hello.html', data = my_data)
+
 
 from markupsafe import escape
 
