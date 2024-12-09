65442589bef200557c46915416abfc5107b21e81
unknown <hernandezgalvezalejandro@gmail.com>
2024-12-08 23:21:19 -0600

    dormularios con wtforms

--- project/first.py
+++ project/first.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
+# clave secreta para el formulario
+app.config.from_mapping(
+    SECRET_KEY = "dev"
+)
 
 # filtros personalizados
 @app.add_template_filter
@@ -57,3 +61,36 @@ from markupsafe import escape
 @app.route("/code/<path:code>")
 def code(code):
     return f"<code>{escape(code)}</code>" # se anula la ejecución de código
+
+# Crear formulario con wtf form
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class Register_Form(FlaskForm):
+    username = StringField("Nombre de Usuario: ", validators = [DataRequired(), Length(min=4, max=25)])
+    password = PasswordField("Contraseña: ", validators = [DataRequired(), Length(min=6, max=40)])
+    submit = SubmitField("Registrar: ")
+
+# registrar usuario
+@app.route("/auth/register", methods = ["GET", "POST"])
+def register():
+    form = Register_Form()
+    print(request.form)
+    if form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
+            return f"Nombre de usuario { username }, Contraseña: {password}"
+            
+    # if request.method == 'POST':
+    #     username = request.form["username"]
+    #     password = request.form["password"]
+        
+    #     if len(username) >= 4 and len(username) <= 25 and len(password) >= 6 and len(password) <= 40:   
+    #         return f"Nombre de usuario { username }, Contraseña: {password}"
+    #     else:
+    #         error = """
+    #         El nombre de usuario debe tener entre 4 y 25 caracteres y la contraseña entre 6 y 40
+    #         """
+    #         return render_template('auth/register.html', form = form, error = error)
+    return render_template('auth/register.html', form = form)
