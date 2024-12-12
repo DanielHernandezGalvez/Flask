6869b43a595755b107887baa23d7de87fe473611
unknown <hernandezgalvezalejandro@gmail.com>
2024-12-11 20:11:26 -0600

    CRUD

--- todo-list/env-todo/todor/auth.py
+++ todo-list/env-todo/todor/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for, redirect, flash
+from flask import Blueprint, render_template, request, url_for, redirect, flash, session, g
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -29,6 +29,54 @@ def register():
             
     return render_template("auth/register.html")
 
-@bp.route("/login")
+@bp.route("/login", methods = ("GET", "POST"))
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+                
+        error = None
+        # validar datos
+        
+        user = User.query.filter_by(username = username).first()
+        if user == None:
+            error = "El usuario no existe"
+        elif not check_password_hash(user.password, password):
+            error = "La contraseña no es correcta"
+        
+        # iniciar sesión
+        if error == None:
+            session.clear()
+            session["user_id"] = user.id
+            return redirect(url_for("todo.index"))
+            
+        flash(error)
+        
     return render_template("auth/login.html")
+
+
+# mantener sesion
+@bp.before_app_request # va a ver si ya se ejecutó la función de abajo
+def load_logger_in_user():
+    user_id = session.get("user_id")
+    
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get_or_404(user_id)
+
+@bp.route("/logout")        
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+import functools 
+
+def login_required(view):
+    @functools.wraps(view) #con esto se decora una función anidada
+    def wrapped_view(**kwargs): # esto son parametros indefinidos
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        return view(**kwargs)
+    return wrapped_view
+    
